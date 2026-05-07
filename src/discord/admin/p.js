a206f98996e0d8c4abb4f394d17d4feb48b59fd4
auieo-dayo <194578811+auieo-dayo@users.noreply.github.com>
2026-05-07 17:01:59 +0900

    sendをいちいち出さない

--- src/discord/admin/p.js
+++ src/discord/admin/p.js
@@ -2,6 +2,6 @@ const { MessageFlags } = require("discord.js")
 function p(bds,message,content){
     message.reply({content:"取得中...",flags:MessageFlags.SuppressNotifications})
     const json = JSON.stringify({"type":"getplayerinfo","playername":content}).replaceAll("\"","'").replaceAll("\\","\\\\'")
-    bds.sendCommand(`send "${json}"`)
+    bds.sendCommand(`send "${json}"`,true)
 }
 module.exports = p

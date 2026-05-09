33d0aaab65dfdb6ff6f5b4413b8ef4e9f7cd7085
auieo-dayo <194578811+auieo-dayo@users.noreply.github.com>
2026-05-09 11:09:08 +0900

    ブロックイベントを50件まで

--- src/discord/admin/block.js
+++ src/discord/admin/block.js
@@ -23,6 +23,7 @@ async function block(message,type,player,block,minutes,logm) {
         params.push(Date.now() - (minutes * 60 * 1000))
     }
     const sql = `SELECT * FROM blockevents ${conditions.length ? `WHERE ${conditions.join(" AND ")}`:""} ORDER BY time DESC LIMIT 30`
+
     const data = logm.db.prepare(sql).all(...params)
     const embed = new EmbedBuilder()
     embed.setTitle("絞り込み結果")
@@ -30,7 +31,8 @@ async function block(message,type,player,block,minutes,logm) {
     let md = ""
     if (!data[0]) embed.setDescription("<Null>"); else {
         const num = (v)=>Number(v).toFixed(0)
-        data.forEach((v)=>{
+        
+        data.splice(0,50).forEach((v)=>{
             const time = new Date(v.time)
             const Type = v.actiontype === Types.blockevents.actiontype.PlaceBlock ? "設置" : "破壊"
             let dim
